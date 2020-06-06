c87d1a6a569319b85c2ae1cb90127072aada765d
Max <fabrinne@fabrinne.com>
2020-06-06 13:40:02 -0300

    deletado o database e arrumando db.js para a manipulação dinâmica do banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -4,9 +4,9 @@ const sqlite3 = require("sqlite3").verbose()
 const db = new sqlite3.Database("./src/database/database.db")
 module.exports = db
 // utilizar o objeto de banco de dados para nossas operações
-/*db.serialize(()=>{
+db.serialize(()=>{
     // construir tabela
-    db.run(`
+    /*db.run(`
         CREATE TABLE IF NOT EXISTS places (
 
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,6 +18,7 @@ module.exports = db
             items TEXT
         );
     `)
+  
     //inserir dados na tabela
     const query = `INSERT INTO places (
         image,
@@ -29,7 +30,7 @@ module.exports = db
     ) VALUES (?,?,?,?,?,?);`
 
     const values = [`
-        "http://www.jnreciclagem.com.br/images/reciclagem.jpg",
+        "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         "papersider",
         "Guilherme Gemballa, Jardim América",
         "N° 260",
@@ -45,10 +46,11 @@ module.exports = db
 
         console.log("cadastrado com sucesso")
         console.log(this)   
-    }
+    }  */
     //db.run(query, values, afterInsertData)
-        
+        const query = `SELECT * FROM places` 
     //consultar dados na tabela
 
     //deletar um dado da tabela
-})*/
+    
+})
